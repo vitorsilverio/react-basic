2d1f265844e0d88489ddb09f3241c2f1c1739b4b
Vitor <vitor.silverio.rodrigues@gmail.com>
2022-04-11 14:31:32 -0300

    Suporte a Temas

---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useCallback, useContext, useMemo, useState } from "react";
+import { Box, ThemeProvider } from "@mui/material";
+
+import { LightTheme, DarkTheme } from "../themes";
+
+interface ThemeContextData {
+  themeName: "light" | "dark";
+  toggleTheme(): void;
+}
+
+const ThemeContext = createContext({} as ThemeContextData);
+
+export const useAppThemeContext = () => {
+    return useContext(ThemeContext)
+};
+
+export const AppThemeProvider: React.FC = ({ children }) => {
+  const [themeName, setThemeName] = useState<"light" | "dark">("light");
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldThemeName) =>
+      oldThemeName === "light" ? "dark" : "light"
+    );
+  }, []);
+  const theme = useMemo(() => {
+    return themeName === "light" ? LightTheme : DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box
+          width="100vw"
+          height="100vh"
+          bgcolor={theme.palette.background.default}
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,5 @@
+import { createTheme } from "@mui/material";
+
+export const LightTheme = createTheme({
+
+});
