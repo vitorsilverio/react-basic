7263ee35234d1d77cfa2beca11f4bed1272d1447
Vitor <vitor.silverio.rodrigues@gmail.com>
2022-04-11 10:37:40 -0300

    Suporte inicial a rotas

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
-export { };
+export * from './home/Home';
+export * from './not-found/NotFound';
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Routes, Route } from "react-router-dom";
+
+import { Home, NotFound } from "../pages";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
