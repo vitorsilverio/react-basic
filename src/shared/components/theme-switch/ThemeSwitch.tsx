0fe516935f932dab8b56087ca2e3882e0caf8e6a
Vitor <vitor.silverio.rodrigues@gmail.com>
2022-04-12 14:44:29 -0300

    Criação do componente para trocar o tema

---
+++ src/shared/components/theme-switch/ThemeSwitch.tsx
@@ -0,0 +1,27 @@
+import { Box, FormLabel, Switch } from '@mui/material';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
+import { useAppThemeContext } from '../../contexts';
+
+export const ThemeSwitch = () => {
+  const { toggleTheme, themeName } = useAppThemeContext();
+  const isDarkTheme = themeName === 'dark';
+
+  return (
+    <Box display='flex' alignItems="center">
+      <FormLabel>
+        <LightModeIcon />
+      </FormLabel>
+      <Switch
+        checked={isDarkTheme}
+        onChange={() => {
+          toggleTheme();
+        }}
+      />
+      <FormLabel>
+        <DarkModeIcon />
+      </FormLabel>
+    </Box>
+  );
+};
--- src/shared/themes/Dark.ts
+++ src/shared/themes/Dark.ts
@@ -2,6 +2,6 @@ import { createTheme } from "@mui/material";
 
 export const DarkTheme = createTheme({
   palette: {
-    mode: "dark",
+    mode: 'dark',
   },
 });
