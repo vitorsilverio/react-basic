05ec7793b978183c209634245ffd5c33d19c98eb
Vítor <vitor.silverio.rodrigues@gmail.com>
2022-06-28 00:16:09 -0300

    fix quotes

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,11 +1,11 @@
-import { Box, Typography } from '@mui/material';
-import { LayoutBase } from '../../shared/layouts';
+import { Box, Typography } from "@mui/material";
+import { LayoutBase } from "../../shared/layouts";
 
 export const NotFound = () => {
   return (
     <LayoutBase titulo='Página não encontrada'>
       <Box>
-        <Typography variant='body1' component='p'>
+        <Typography variant="body1" component="p">
           Erro 404 - O recurso especificado não existe.
         </Typography>
       </Box>
